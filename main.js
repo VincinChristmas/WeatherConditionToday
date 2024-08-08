a7e7d26459e48ef4757685b611cf419b2bb23568
VincinChristmas <VincinChristmas@hotmail.com>
2024-08-08 14:32:13 +0800

    added the JavaScript for the weather app today

--- main.js
+++ main.js
@@ -1,17 +1,43 @@
-document.querySelector('button').addEventListener('click', getFact);
+//document.querySelector('button').addEventListener('click', getFact);
 
-function getFact() {
+function getWeather(){
 
 
 fetch("https://api.open-meteo.com/v1/forecast?latitude=32.0617&longitude=118.7778&current=temperature_2m&hourly=temperature_2m&timezone=auto") 
 .then(res => res.json())
 .then(data => {
     console.log(data)
-    document.querySelector('h2').innerText = `${data.current.temperature_2m} ${data.current_units.temperature_2m
-    }`
+    let temperatures = data.hourly.temperature_2m
+    let lowestTemperatures = []
+    let highestTemperatures = []
+
+    
+    function findLowestTemperature(tempArray) {
+        return Math.min(...tempArray);
+      }
+
+    function findHighestTemperature(tempArray) {
+        return Math.max(...tempArray)
+    }
+
+      for (let i = 0; i < temperatures.length; i += 24) {
+        let current24Hours = temperatures.slice(i, i + 24)
+        let lowestTemp = findLowestTemperature(current24Hours)
+        lowestTemperatures.push(` ${lowestTemp}${data.current_units.temperature_2m} `)
+      }
+    document.querySelector('.lows').innerText = ` ${lowestTemperatures.join('')} `
+
+    for (let i = 0; i < temperatures.length; i += 24) {
+        let current24Hours = temperatures.slice(i, i + 24)
+        let highestTemp = findHighestTemperature(current24Hours)
+        highestTemperatures.push(` ${highestTemp}${data.current_units.temperature_2m} `)
+      }
+    document.querySelector('.highs').innerText = `${highestTemperatures.join('')}`
     
 })
 .catch(err => {
     console.log(`error ${err}`)
 });
 }
+
+getWeather()
