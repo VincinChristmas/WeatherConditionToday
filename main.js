b4c21bbb46b3f17f659e1ee02f0b706a7fe6d1d3
VincinChristmas <VincinChristmas@hotmail.com>
2024-08-12 11:17:31 +0800

    added new code to the weather app today

--- main.js
+++ main.js
@@ -25,14 +25,30 @@ fetch("https://api.open-meteo.com/v1/forecast?latitude=32.0617&longitude=118.777
         let lowestTemp = findLowestTemperature(current24Hours)
         lowestTemperatures.push(` ${lowestTemp}${data.current_units.temperature_2m} `)
       }
-    document.querySelector('.lows').innerText = `${lowestTemperatures.join('  ')}`
+    document.querySelector('.lowOne').innerText = `${lowestTemperatures[0]}`
+    document.querySelector('.lowTwo').innerText = `${lowestTemperatures[1]}`
+    document.querySelector('.lowThree').innerText = `${lowestTemperatures[2]}`
+    document.querySelector('.lowFour').innerText = `${lowestTemperatures[3]}`
+    document.querySelector('.lowFive').innerText = `${lowestTemperatures[4]}`
+    document.querySelector('.lowSix').innerText = `${lowestTemperatures[5]}`
+    document.querySelector('.lowSeven').innerText = `${lowestTemperatures[6]}`
+
+    
+
 
     for (let i = 0; i < temperatures.length; i += 24) {
         let current24Hours = temperatures.slice(i, i + 24)
         let highestTemp = findHighestTemperature(current24Hours)
         highestTemperatures.push(` ${highestTemp}${data.current_units.temperature_2m} `)
       }
-    document.querySelector('.highs').innerText = `${highestTemperatures.join('  ')}`
+      document.querySelector('.highOne').innerText = `${highestTemperatures[0]}`
+      document.querySelector('.highTwo').innerText = `${highestTemperatures[1]}`
+      document.querySelector('.highThree').innerText = `${highestTemperatures[2]}`
+      document.querySelector('.highFour').innerText = `${highestTemperatures[3]}`
+      document.querySelector('.highFive').innerText = `${highestTemperatures[4]}`
+      document.querySelector('.highSix').innerText = `${highestTemperatures[5]}`
+      document.querySelector('.highSeven').innerText = `${highestTemperatures[6]}`
+  
     
 })
 .catch(err => {
